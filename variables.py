74d9a6e2927a89fe39aef8a7dfd6cc4517dc5b14
K Kiran Kumar <113544336+kiran150502@users.noreply.github.com>
2022-09-21 21:54:33 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,33 @@
+# print(10.7)
+
+# a=7
+# b=2
+# b=8
+# print(b)
+# b=3
+# print(b)
+
+# a=10
+# b=20
+# c=30
+# print(b)
+
+# a=10 ;  b=20 ; c=30
+# a,b,c=10,20,30
+# print(a,b,c)
+# print(a,b,a)
+
+
+a,b,c,d=10,20,30
+
+# a,b=3,4,5
+
+
+
+
+
+
+# a=6,7
+# print(type(a))
+
+
